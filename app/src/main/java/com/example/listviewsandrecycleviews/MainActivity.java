08ea09e9aee64e8f98fa8e30337f21cfe8b9e000
Zain <saeedzain20@gmail.com>
2021-08-25 18:17:31 +0500

    Added onClickListener for builtin adapter

--- app/src/main/java/com/example/listviewsandrecycleviews/MainActivity.java
+++ app/src/main/java/com/example/listviewsandrecycleviews/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.listviewsandrecycleviews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +19,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         listView1 = findViewById(R.id.listView1);
         //Using built in array adapter
-//        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arr);
-//        listView1.setAdapter(ad);
-
-//        Using myAdapter
-        MyAddapter ad = new MyAddapter(this, R.layout.my_layout, arr);
+        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arr);
         listView1.setAdapter(ad);
+
+//      Using myAdapter
+//      MyAddapter ad = new MyAddapter(this, R.layout.my_layout, arr);
+//      listView1.setAdapter(ad);
+
+        listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "You clicked on: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
     }
 }
